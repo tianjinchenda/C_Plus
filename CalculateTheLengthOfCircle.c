4ab32ed9d29abfa966e099848c14eae8b3032fad
tianjinchenda <tianjinchenda@sina.com>
2019-06-06 22:21:17 +0800

    Add files via upload

---
+++ CalculateTheLengthOfCircle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	printf("This Program will Calculate the Length of a Circle\n");
+	printf("It will need you in the Radis of the Circle\n");
+	printf("Please input the Radis of the Circle below\n");
+	float result;
+	float radius;
+	scanf("%g",&radius);
+	result = radius * 2 * 3.14;
+	printf("The Length of the Circle is %g\n",result);
+	return 0;
+	
+}
