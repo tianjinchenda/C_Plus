de50b1a1299c3aab4e42b73d6ae25a9474ba3205
tianjinchenda <tianjinchenda@sina.com>
2019-07-06 23:13:37 +0800

    Add files via upload

---
+++ if.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int num = 0;
+	printf("Please input a number");
+	scanf("%d" , &num);
+	if (num>0){
+	printf("postivie");
+	}	
+	else if(num<0){
+	printf("negative");	
+}
+}
